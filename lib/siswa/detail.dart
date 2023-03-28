d02d0665c337cf59f1e521d64fd11c1e5c471564
dhita25 <dhitafadillah2004@gmail.com>
2023-03-28 00:55:37 -0700

    student

---
+++ lib/siswa/detail.dart
@@ -0,0 +1,152 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/material.dart';
+import 'package:spp/controllers/kelas_controller.dart';
+import 'package:spp/controllers/pembayaran_controller.dart';
+import 'package:spp/models/kelas_model.dart';
+import 'package:spp/models/pembayaran_model.dart';
+import '../compenents/mytetxtfield.dart';
+
+class ManageEntri extends StatefulWidget {
+  final pembayaran_model? pembayaran;
+  final index;
+  ManageEntri({this.pembayaran, this.index});
+
+  @override
+  State<ManageEntri> createState() => _ManageEntriState();
+}
+
+class _ManageEntriState extends State<ManageEntri> {
+  final _form_key = GlobalKey<FormState>();
+  bool iseditingmode = false;
+  final TextEditingController id= TextEditingController();
+  final TextEditingController nis= TextEditingController();
+  final TextEditingController jumlah_bayar= TextEditingController();
+  final TextEditingController tanggal_bayar= TextEditingController();
+  final TextEditingController bulan_bayar= TextEditingController();
+  final TextEditingController tahun_bayar= TextEditingController();
+  final TextEditingController status= TextEditingController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    if(widget.index != null){
+      iseditingmode = true;
+      id.text = widget.pembayaran?.id;
+      nis.text = widget.pembayaran?.nis;
+      jumlah_bayar.text = widget.pembayaran?.jumlah_bayar;
+      tanggal_bayar.text = widget.pembayaran?.tanggal_bayar;
+      bulan_bayar.text = widget.pembayaran?.bulan_bayar;
+      tahun_bayar.text = widget.pembayaran?.tahun_bayar;
+      status.text = widget.pembayaran?.status;
+    }else{
+      iseditingmode = false;
+
+    }
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Detail pembayaran'),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(height: 50),
+              Center(
+                child: 
+                iseditingmode == true
+                ? const Text(
+                  "DETAIL PEMBAYARAN", 
+                  style: TextStyle(fontSize: 28),
+                  selectionColor: Colors.black,
+                  )
+                : const Text(
+                  "ENTRI PEMBAYARAN", 
+                  style: TextStyle(
+                    fontSize: 28,
+                    color: Colors.black,
+                    ),
+                  ),
+              ),
+              const SizedBox(height: 30),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Form(
+                  key: _form_key,
+                  child: Column(
+                    children: [
+                      MyTextField(
+                        hintedtex: "example: 20218957", 
+                        labledtext: "NIS", 
+                        mycontroller: nis,),
+                      const SizedBox(height: 10),
+                      MyTextField(
+                        hintedtex: "example: 70000", 
+                        labledtext: "jumlah bayar", 
+                        mycontroller: jumlah_bayar,),
+                      const SizedBox(height: 10),
+                      MyTextField(
+                        hintedtex: "example: tanggal sekarang", 
+                        labledtext: "tamnggal bayar", 
+                        mycontroller: tanggal_bayar,),
+                      const SizedBox(height: 10),
+                      MyTextField(
+                        hintedtex: "example: januari", 
+                        labledtext: "bulan bayar", 
+                        mycontroller: tanggal_bayar,),
+                      const SizedBox(height: 10),
+                      MyTextField(
+                        hintedtex: "example:2023", 
+                        labledtext: "Tahun Bayar", 
+                        mycontroller: tahun_bayar,),
+                      const SizedBox(height: 10),
+                      MyTextField(
+                        hintedtex: "example: Lunas / Tunggakan 100000", 
+                        labledtext: "status", 
+                        mycontroller: status,),
+                      const SizedBox(height: 10),
+                    ],
+                )),
+              ),
+              const SizedBox(height: 10),
+              // ElevatedButton(
+              //   onPressed: () {
+              //   if(_form_key.currentState!.validate()){
+              //     _form_key.currentState!.save();
+              //         if(iseditingmode == true){
+              //           pembayaran_controller().update_pembayaran(
+              //             pembayaran_model( 
+              //               id: id.text,
+              //               nis: nis.text,
+              //               jumlah_bayar: jumlah_bayar.text, 
+              //               tanggal_bayar: tanggal_bayar.text, 
+              //               bulan_bayar: bulan_bayar.text, 
+              //               tahun_bayar: tahun_bayar.text, 
+              //               status: status.text));
+              //         }
+              //         else{
+              //           pembayaran_controller().add_pembayaran(
+              //             pembayaran_model( 
+              //               nis: nis.text,
+              //               jumlah_bayar: jumlah_bayar.text, 
+              //               tanggal_bayar: tanggal_bayar.text, 
+              //               bulan_bayar: bulan_bayar.text, 
+              //               tahun_bayar: tahun_bayar.text, 
+              //               status: status.text));
+              //         }
+              //   }
+                
+              // }, child: iseditingmode == true ? Text("update") : Text("simpan") )
+
+            ],),
+        )),
+    );
+  }
+}
+
--- lib/student.dart
+++ lib/student.dart
@@ -1,48 +1,94 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:spp/screans/studentPage.dart';
+import 'package:spp/siswa/history.dart';
+import 'package:spp/controllers/logout.dart';
 
-import 'login.dart';
-
-class Student extends StatefulWidget {
-  const Student({super.key});
+class Student extends StatefulWidget{
+  const Student ({super.key});
 
   @override
   State<Student> createState() => _StudentState();
 }
 
-class _StudentState extends State<Student> {
+class _StudentState extends State<Student>{
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
+  Widget build(BuildContext context){
+    return Scaffold( 
       appBar: AppBar(
         backgroundColor: Colors.blueGrey[900],
         title: Text("Student"),
-        actions: [
-          IconButton(
-            onPressed: () {
-              logout(context);
-            },
-            icon: Icon(
-              Icons.logout,
+        // actions: [
+        //   IconButton(
+        //     onPressed: (){
+        //       logout(context);
+        //     }, 
+        //      icon: Icon(
+        //       Icons.logout,
+        //      ),
+        //      )
+        // ],
+      ),
+      //data data sidebar
+      body: GridView.count(
+        padding: const EdgeInsets.all(25),
+        crossAxisCount: 1,
+        children: <Widget> [
+          //history
+          Card(
+            margin: const EdgeInsets.all(8),
+            color: Colors.blue,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10)
             ),
-          )
+            child: InkWell(
+              onTap: (){Navigator.push(context, MaterialPageRoute(builder: (context) => const PembayaranPage()));},
+              splashColor: Colors.white,
+              child: Center(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: const <Widget> [
+                    Icon(Icons.history, size: 70, color: Colors.white,),
+                    Text("HISTORY", style: TextStyle(fontSize: 17.0, color: Colors.white))
+                  ],
+                ),
+              ),
+            ),
+          ),
+          //LOGOUT
+          Card(
+            margin: const EdgeInsets.all(8),
+            color: Colors.orange[400],
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10)
+            ),
+            child: InkWell(
+              onTap: (){
+                logout(context);
+              },
+              splashColor: Colors.white,
+              child: Center(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: const <Widget> [
+                    Icon(Icons.logout, size: 70, color: Colors.white,),
+                    Text("LOGOUT", style: TextStyle(fontSize: 17.0, color: Colors.white)),
+                  ],
+                ),
+              ),
+            ),
+          ),
         ],
-      ),
-      body: Container(
-        color: Colors.white,
-      ),
-    );
-  }
-
-  Future<void> logout(BuildContext context) async {
-    CircularProgressIndicator();
-    await FirebaseAuth.instance.signOut();
-    Navigator.pushReplacement(
-      context,
-      MaterialPageRoute(
-        builder: (context) => LoginPage(),
       ),
     );
   }
 }
+
+// Future<void> logout(BuildContext context) async{
+//   CircularProgressIndicator();
+//   await FirebaseAuth.instance.signOut();
+//   Navigator.pushReplacement(
+//     context, 
+//     MaterialPageRoute(
+//       builder: (context) => LoginPage(),
+//       ),
+//       );
+// }
